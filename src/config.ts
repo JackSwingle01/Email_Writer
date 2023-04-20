589b83055a5f387fa195f84974cb20f1b965e85c
jackswingle01 <jack774411@gmail.com>
2023-04-20 12:23:24 -0400

    removed unused comments

--- src/config.ts
+++ src/config.ts
@@ -1,11 +1,6 @@
-// import { defineConfig } from 'astro/config';
 
 export const SITE_TITLE = "Email Generator";
 export const SITE_DESCRIPTION = "Create emails using AI.";
 export const SITE_AUTHOR = "Team 9"
 
 export const DEVELOPMENT_MODE = false; // set to false before pushing to github!
-
-// export default defineConfig({
-//     output: 'server'
-// });
